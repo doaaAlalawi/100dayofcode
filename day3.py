2772c034ae28c6190b38f436d4b95de64ff49902
unknown <Doaa.alalawi@outlook.sa>
2019-08-23 00:37:09 +0300

    3rd day task

---
+++ day3.py
@@ -0,0 +1,50 @@
+"""
+# examplle 1 
+x = 5 
+y = "John"
+print (x)
+print (y)
+
+# example 2 
+x = 4 # x is of type int 
+x = "Sally" # x is now of type str 
+print (x)
+
+
+# example 3 
+x = " Johan"
+# is the same as 
+x = 'John'
+print (x)
+
+# example 4
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# example 5
+x= y= z = "Orange"
+print(x)
+print(y)
+print(z)
+
+# example 6
+x = "awesome"
+print ("Python is "+ x)
+
+# example 7
+x= "Python is "
+y = "awesome"
+z = x + y
+print (z)
+
+# example 8
+x = 5 
+y = 10 
+print (x + y)
+"""
+# example 9
+x = 5 
+y = "John"
+print (x + y)
