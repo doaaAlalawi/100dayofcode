ea1013703348d816a0300f31e153b93d687decfa
unknown <Doaa.alalawi@outlook.sa>
2019-08-23 23:59:35 +0300

    task 4

---
+++ day4.py
@@ -0,0 +1,24 @@
+"""
+
+# example 1 
+x=1 
+y=2.8
+z=1j
+print (type(x))
+print (type(y))
+print (type(z))
+
+# example 2 
+x=1 
+y=2.8
+z=1j
+a = float(x)
+b = int(y)
+c = complex(x)
+print (type(a))
+print (type(b))
+print (type(c))
+"""
+# example 3
+import random 
+print (random.randrange(1,10))
